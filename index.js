15d70ee445c5daa2eede6bba85b7a86f17406084
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-29 16:58:08 -0400

    forEach method

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const someNames = ["Petro", "Vasyl", "Ivan"];
+ const someItems = ["Hello!", "O bozhe jake konchene", "ty-ty-ty"];
 
-someNames.unshift("Puzdlo");
-someNames.push(1,2,3);
-someNames.splice(2, 0, "aaa")
+ someItems.forEach((item, index) => {
+   console.log(index, item);
+ });
 
-console.log(someNames);
-
-
-const someNumbers = [1, 2, 3, 4, 7, 8, 9];
-
-someNumbers.splice(4, 0, 5,6);
-
-console.log(someNumbers);
+ 
