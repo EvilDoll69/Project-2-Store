62786a9c5bd69a236eb179db9165e69ab2bf3d02
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-29 16:26:01 -0400

    More practice

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const someNames = ["Petro", "Vasyl", "Ivan"];
 
+someNames.unshift("Puzdlo");
+someNames.push(1,2,3);
+someNames.splice(2, 0, "aaa")
+
 console.log(someNames);
+
+
+const someNumbers = [1, 2, 3, 4, 7, 8, 9];
+
+someNumbers.splice(4, 0, 5,6);
+
+console.log(someNumbers);
